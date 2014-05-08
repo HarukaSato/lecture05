8cec50089778edb51faaec757ca88f75793fffab
HarukaSato <sundy0315@gmail.com>
2014-05-08 12:56:10 +0900

    いろいろ変えた笑

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var inputoperandA = document.querySelector("#operand_a");
 var inputoperandB = document.querySelector("#operand_b");
-var inputOperator = document.querySelector("#operator");
 var output = document.querySelector("#output");
 var error = document.querySelector("#error");
 
@@ -12,56 +11,40 @@ var hideError = function(){
 	error.setAttribute("class", "hidden");
 };
 
+var devide = function(a, b){
+	b = 1 - b / 100;
+	return a * b;
+};
+
 var showResult = function(result){
 	output.value = result + "";
-};
-
-
-var divide = function(a, b){
-	return a / b;
-};
-
-
-var isOperator = function(operator){
-	return 	operator == "/" || operator == "%";
 };
 
 var isNumber = function(a){
 	return !Number.isNaN(a);
 };
-
-var isNonZeroNumber = function(a){
-	return isNumber(a) && a != 0;
+/*
+var Bnumber = function(b){
+	return !Number.isNaN(b);
 };
 
-var isInteger = function(a){
-	return Number.isInteger(a);
+var bnumber = function(b){
+	return 0<Bnumber(b)<=100;
 };
-
-var isNaturalNumber = function(a){
-	return isInteger(a) && a > 0;
-};
-
-var isDivision = function(operator, a, b){
-	return operator == "/" && isNumber(a) && isNonZeroNumber(b);
-};
-
-}
-
-var isReady = function(operator, a, b){
-	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
-};
-
+*/
 var startCalc = function(){
 	var operandA = inputoperandA.value;
 	var operandB = inputoperandB.value;
-	var operator = inputOperator.value;
 
 	operandA = Number(operandA);
 	operandB = Number(operandB);
 
-	
+	var result = 0;
+	result = devide(operandA, operandB);
+	showResult(result);
+
+};
+
 
 var initApp = function(){
 	var calcButton = document.querySelector("#calcButton");
